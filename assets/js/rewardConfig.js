a11717d7b4f277268d9f30a5066273a6e97c38bf
ace <343656921@qq.com>
2015-08-14 20:13:37 +0800

    配置管理

--- assets/js/rewardConfig.js
+++ assets/js/rewardConfig.js
@@ -1,6 +1,18 @@
 $(function() {
     var $multiCheckbox = $('th input[type="checkbox"]'), //多选
         $singleCheckbox = $('td input[type="checkbox"]'), //单选
+        rewardStatus = {
+            on: {
+                code: '1',
+                name: '开启',
+                className: 'toggle-on'
+            }, //奖励开启
+            off: {
+                code: '0',
+                name: '关闭',
+                className: 'toggle-off'
+            } //奖励关闭
+        },
         /**
          * 删除品牌
          * @param  {[type]}   ids      [description]
@@ -68,7 +80,30 @@ $(function() {
             location.href = href + '?id=' + id;
         }
     });
-
+    /**
+     * 改变状态
+     * @param  {[type]} 
+     * @return {[type]}   [description]
+     */
+    $('.J_change_status').on('click', function() {
+        var $this = $(this),
+            status = $this.attr('data-status'),
+            toggleStatus = status === rewardStatus.on.code ? rewardStatus.off : rewardStatus.on,
+            id = $this.closest('tr').attr('data-id');
+        $.post('', {
+            id: id,
+            status: toggleStatus.code
+        }).then(function(res) {
+            if (res.code == 200) {
+                notify.warn(toggleStatus.code + '成功');
+                $this.removeClass(function(i, classNames) {
+                    return classNames.replace('J_change_status', '');
+                }).addClass(toggleStatus.className).attr('data-status', toggleStatus.code);
+            } else {
+                notify.warn(toggleStatus.code + '失败');
+            }
+        });
+    });
     /**
      * 提交添加编辑奖励
      * @param  {[type]} 
@@ -86,8 +121,8 @@ $(function() {
             des = $.trim($rewardDes.val()),
             rule = $.trim($rewardRule.val());
         if (name == '') {
-            notity.warn('奖励名称不能为空');
-            $rewardName.facus();
+            notify.warn('奖励名称不能为空');
+            $rewardName.focus();
             return;
         }
         $('.J_reward_form').ajaxSubmit({
